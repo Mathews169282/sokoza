a69fd9087f1b2780d89d79c4be9cf8988eebfc26
Mathews169282 <mathews.zimba@strathmore.edu>
2026-07-08 14:48:38 +0300

    Update favicon to Sokoza logo

--- src/components/sokoza/Logo.tsx
+++ src/components/sokoza/Logo.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Logo: React.FC<{ light?: boolean }> = ({ light }) => (
   <a href="#home" className="flex items-center select-none">
     <img
-      src="/sokoza logo.png"
+      src="/sokoza-logo.png"
       alt="Sokoza Events"
       className="h-12 w-auto"
     />
